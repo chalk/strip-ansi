14d51de47776e01ab1ef566347fba2d320719cea
Benjamin White <ext-ben.white@nokia.com>
2014-03-26 14:13:11 +0000

    Strip arbitrary-length escape sequences

--- test.js
+++ test.js
@@ -7,6 +7,10 @@ it('should strip color from string', function () {
 	assert.equal(strip('\x1b[0m\x1b[4m\x1b[42m\x1b[31mfoo\x1b[39m\x1b[49m\x1b[24mfoo\x1b[0m'), 'foofoo');
 });
 
+it('should strip reset;setfg;setbg;italics;strike;underline sequence from string', function () {
+	assert.equal(strip('\x1b[0;33;49;3;9;4mbar\x1b[0m'), 'bar');
+});
+
 it('should strip color with CLI', function (done) {
 	exec('echo "\x1b[0m\x1b[4m\x1b[42m\x1b[31mfoo\x1b[39m\x1b[49m\x1b[24mfoo\x1b[0m" | ./cli.js', function (err, stdout) {
 		assert.equal(stdout, 'foofoo\n');
