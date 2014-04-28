683be599335d9f9789e7cfad06960173e7d877a0
Fotis Gimian <fgimiansoftware@gmail.com>
2014-04-28 12:05:27 +0200

    Close GH-3: Repaired a bug whereby color was not correctly stripped from the ls command.

--- test.js
+++ test.js
@@ -7,6 +7,10 @@ it('should strip color from string', function () {
 	assert.equal(strip('\x1b[0m\x1b[4m\x1b[42m\x1b[31mfoo\x1b[39m\x1b[49m\x1b[24mfoo\x1b[0m'), 'foofoo');
 });
 
+it('should strip color from ls command', function () {
+    assert.equal(strip('\x1b[00;38;5;244m\x1b[m\x1b[00;38;5;33mfoo\x1b[0m'), 'foo');
+});
+
 it('should strip reset;setfg;setbg;italics;strike;underline sequence from string', function () {
 	assert.equal(strip('\x1b[0;33;49;3;9;4mbar\x1b[0m'), 'bar');
 });
