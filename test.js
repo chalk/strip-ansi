afd80021ef59354b66c742c161c58682c0b4c450
Sindre Sorhus <sindresorhus@gmail.com>
2014-06-24 21:33:23 +0200

    bump ansi-regex

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
 'use strict';
 var assert = require('assert');
 var exec = require('child_process').exec;
-var strip = require('./index');
+var strip = require('./');
 
 it('should strip color from string', function () {
-	assert.equal(strip('\x1b[0m\x1b[4m\x1b[42m\x1b[31mfoo\x1b[39m\x1b[49m\x1b[24mfoo\x1b[0m'), 'foofoo');
+	assert.equal(strip('\u001b[0m\u001b[4m\u001b[42m\u001b[31mfoo\u001b[39m\u001b[49m\u001b[24mfoo\u001b[0m'), 'foofoo');
 });
 
 it('should strip color from ls command', function () {
-    assert.equal(strip('\x1b[00;38;5;244m\x1b[m\x1b[00;38;5;33mfoo\x1b[0m'), 'foo');
+    assert.equal(strip('\u001b[00;38;5;244m\u001b[m\u001b[00;38;5;33mfoo\u001b[0m'), 'foo');
 });
 
 it('should strip reset;setfg;setbg;italics;strike;underline sequence from string', function () {
 	assert.equal(strip('\x1b[0;33;49;3;9;4mbar\x1b[0m'), 'bar');
 });
 
-it('should strip color with CLI', function (done) {
-	exec('echo "\x1b[0m\x1b[4m\x1b[42m\x1b[31mfoo\x1b[39m\x1b[49m\x1b[24mfoo\x1b[0m" | ./cli.js', function (err, stdout) {
+it('should strip color with CLI', function (cb) {
+	exec('echo "\u001b[0m\u001b[4m\u001b[42m\u001b[31mfoo\u001b[39m\u001b[49m\u001b[24mfoo\u001b[0m" | ./cli.js', function (err, stdout) {
 		assert.equal(stdout, 'foofoo\n');
-		done();
+		cb();
 	});
 });
