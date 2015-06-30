c0a1a7db94eb593f1ba2e133cf53da8a363a31e6
Sindre Sorhus <sindresorhus@gmail.com>
2015-06-30 18:52:47 +0200

    move CLI into a separate module
    https://github.com/sindresorhus/strip-ansi-cli

--- test.js
+++ test.js
@@ -1,33 +1,17 @@
 'use strict';
-var assert = require('assert');
-var exec = require('child_process').exec;
-var strip = require('./');
+var test = require('ava');
+var fn = require('./');
 
-it('should strip color from string', function () {
-	assert.equal(strip('\u001b[0m\u001b[4m\u001b[42m\u001b[31mfoo\u001b[39m\u001b[49m\u001b[24mfoo\u001b[0m'), 'foofoo');
+test('strip color from string', function (t) {
+	t.assert(fn('\u001b[0m\u001b[4m\u001b[42m\u001b[31mfoo\u001b[39m\u001b[49m\u001b[24mfoo\u001b[0m') === 'foofoo');
+	t.end();
 });
 
-it('should strip color from ls command', function () {
-    assert.equal(strip('\u001b[00;38;5;244m\u001b[m\u001b[00;38;5;33mfoo\u001b[0m'), 'foo');
+test('strip color from ls command', function (t) {
+	t.assert(fn('\u001b[00;38;5;244m\u001b[m\u001b[00;38;5;33mfoo\u001b[0m') === 'foo');
+	t.end();
 });
-
-it('should strip reset;setfg;setbg;italics;strike;underline sequence from string', function () {
-	assert.equal(strip('\x1b[0;33;49;3;9;4mbar\x1b[0m'), 'bar');
-});
-
-it('should strip color with CLI', function (cb) {
-	exec('echo "\u001b[0m\u001b[4m\u001b[42m\u001b[31mfoo\u001b[39m\u001b[49m\u001b[24mfoo\u001b[0m" | ./cli.js', function (err, stdout) {
-		assert.equal(stdout, 'foofoo\n');
-		cb();
-	});
-});
-
-it('should strip color from file with CLI', function (cb) {
-	exec('echo "\u001b[0m\u001b[4m\u001b[42m\u001b[31mfoo\u001b[39m\u001b[49m\u001b[24mfoo\u001b[0m" > colored.example.txt', function (err, stdout) {
-    if (err) cb(err);
-    exec('./cli.js colored.example.txt', function (err, stdout) {
-      assert.equal(stdout, 'foofoo\n');
-      cb(err);
-    });
-  })
+test('strip reset;setfg;setbg;italics;strike;underline sequence from string', function (t) {
+	t.assert(fn('\x1b[0;33;49;3;9;4mbar\x1b[0m') === 'bar');
+	t.end();
 });
