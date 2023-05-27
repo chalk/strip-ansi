d54a07a538cf9cc5c8fbb630416707d70ad21716
Sindre Sorhus <sindresorhus@gmail.com>
2023-05-27 21:45:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import ansiRegex from 'ansi-regex';
 
-const re = ansiRegex();
+const regex = ansiRegex();
+
 export default function stripAnsi(string) {
 	if (typeof string !== 'string') {
 		throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
 	}
 
-	return string.replace(re, '');
+	return string.replace(regex, '');
 }
