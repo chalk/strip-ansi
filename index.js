f2b3e136f31b24b35f749e1a2ecd5ff643ff3aa0
gtmnayan <gtmnayan@gmail.com>
2023-05-27 20:13:54 +0545

    perf: only create regex once

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import ansiRegex from 'ansi-regex';
 
+const re = ansiRegex();
 export default function stripAnsi(string) {
 	if (typeof string !== 'string') {
 		throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
 	}
 
-	return string.replace(ansiRegex(), '');
+	return string.replace(re, '');
 }
