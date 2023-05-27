70aa3575c924e149ab85f655ae4424dd428aacfc
gtmnayan <gtmnayan@gmail.com>
2023-05-27 21:18:08 +0545

    remark: perf penalty for reset

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ export default function stripAnsi(string) {
 		throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
 	}
 
+	// Even though the regex is global, we don't need to reset the lastIndex
+	// because unlike exec/test, replace does it automatically and doing it manually
+	// has a perf penalty.
 	return string.replace(regex, '');
 }
